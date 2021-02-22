3f2ac07ac05c303d5dc2b73d99051b133022cc44
0xBahamoot <lam@autonomous.nyc>
2021-02-22 16:09:03 +0700

    update

--- src/api_signSchnorr.c
+++ src/api_signSchnorr.c
@@ -14,31 +14,31 @@ static uint8_t set_result_sign_schnorr(uint8_t sig_size)
     return tx;
 }
 
-void handleSignSchnorr(uint8_t p1, uint8_t p2, uint8_t *dataBuffer, uint16_t dataLength, volatile unsigned int *flags, volatile unsigned int *tx)
+void handleSignSchnorr(uint8_t p1, uint8_t p2, uint8_t* dataBuffer, uint16_t dataLength, volatile unsigned int* flags, volatile unsigned int* tx)
 {
     UNUSED(dataLength);
     UNUSED(p2);
     UNUSED(p1);
     os_memset(processData, 0, sizeof(processData));
 
-    union api_signSchnorr
+    union ugroup1
     {
         unsigned char pedPrivate[32]; //publicKey.g
         unsigned char pedRandom[32];  //publicKey.h
         unsigned char randomness[32]; //r
+        unsigned char randomnumber[32]; 
     } U;
 
     unsigned char message[64];
-    unsigned char e[32];
     unsigned char s1[32];
     unsigned char z1[32];
 
     os_memmove(U.randomness, dataBuffer + 64, 32);
-
     if (cx_math_is_zero(U.randomness, 32) != 0)
     {
-        cx_rng(U.randomness, 32);
-        incognito_hash_to_scalar(s1, U.randomness, 32);
+        unsigned char e[32];
+        cx_rng(U.randomnumber, 32);
+        incognito_hash_to_scalar(s1, U.randomnumber, 32);
 
         os_memmove(U.pedPrivate, dataBuffer + 32, 32);
         incognito_ecmul_k(message, U.pedPrivate, s1);
@@ -57,40 +57,49 @@ void handleSignSchnorr(uint8_t p1, uint8_t p2, uint8_t *dataBuffer, uint16_t dat
     }
     else
     {
-        // unsigned char s2[32];
-        // unsigned char t[32];
-        // unsigned char t1[32];
-        // unsigned char z2[32];
+        unsigned char s2[32];
+        union ugroup2
+        {
+            unsigned char t1[32];
+            unsigned char z2[32];
+        } U2;
 
-        // cx_rng(U.randomness, 32);
-        // incognito_hash_to_scalar(s1, U.randomness, 32);
-        // cx_rng(U.randomness, 32);
-        // incognito_hash_to_scalar(s2, U.randomness, 32);
+        union ugroup3
+        {
+            unsigned char t[32];
+            unsigned char e[32];
+        } U3;
 
-        // os_memmove(U.pedPrivate, dataBuffer + 32, 32);
-        // incognito_ecmul_k(t, U.pedPrivate, s1);
+        cx_rng(U.randomnumber, 32);
+        incognito_hash_to_scalar(s1, U.randomnumber, 32);
+        cx_rng(U.randomnumber, 32);
+        incognito_hash_to_scalar(s2, U.randomnumber, 32);
 
-        // os_memmove(U.pedRandom, dataBuffer + 32, 32);
-        // incognito_ecmul_k(t1, U.pedRandom, s2);
+        os_memmove(U.pedPrivate, dataBuffer + 32, 32);
+        incognito_ecmul_k(U3.t, U.pedPrivate, s1);
 
-        // incognito_addm(t, t, t1);
-        // os_memmove(message, t, 32);
-        // os_memmove(message + 32, dataBuffer + 96, 32);
+        os_memmove(U.pedRandom, dataBuffer, 32);
+        incognito_ecmul_k(U2.t1, U.pedRandom, s2);
 
-        // incognito_hash_to_scalar(e, message, 64);
+        incognito_ecadd(U3.t, U3.t, U2.t1);
 
-        // incognito_multm(U.pedPrivate, G_crypto_state_t.key.key, e);
-        // incognito_subm(z1, s1, U.pedPrivate);
+        os_memmove(message, U3.t, 32);
+        os_memmove(message + 32, dataBuffer + 96, 32);
 
-        // os_memmove(U.randomness, dataBuffer + 64, 32);
-        // incognito_multm(s1, U.randomness, e);
-        // incognito_subm(z2, s2, s1);
+        incognito_hash_to_scalar(U3.e, message, 64);
 
-        // os_memmove(processData, e, 32);
-        // os_memmove(processData + 32, z1, 32);
-        // os_memmove(processData + 64, z2, 32);
+        incognito_multm(U.pedPrivate, G_crypto_state_t.key.key, U3.e);
+        incognito_subm(z1, s1, U.pedPrivate);
 
-        // sendResponse(set_result_sign_schnorr(96), true);
-        // return;
+        os_memmove(U.randomness, dataBuffer + 64, 32);
+        incognito_multm(U2.z2, U.randomness, U3.e);
+        incognito_subm(U2.z2, s2, U2.z2);
+
+        os_memmove(processData, U3.e, 32);
+        os_memmove(processData + 32, z1, 32);
+        os_memmove(processData + 64, U2.z2, 32);
+
+        sendResponse(set_result_sign_schnorr(96), true);
+        return;
     }
 }
