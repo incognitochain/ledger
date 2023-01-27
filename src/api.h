c9892e7992a99fec9d264db86a5f227a0912544c
darius <datle@incognito.org>
2023-01-27 15:11:23 +0700

    remove debug methods (set alpha) from review branch

--- src/api.h
+++ src/api.h
@@ -41,8 +41,6 @@ void handleCalculateC(uint8_t p1, uint8_t p2, uint8_t *dataBuffer, uint16_t data
 
 void handleCalculateCCA(uint8_t p1, uint8_t p2, uint8_t *dataBuffer, uint16_t dataLength, volatile unsigned int *flags, volatile unsigned int *tx);
 
-void handleSetAlpha(uint8_t p1, uint8_t p2, uint8_t *dataBuffer, uint16_t dataLength, volatile unsigned int *flags, volatile unsigned int *tx);
-
 void handleDecryptCoin(uint8_t p1, uint8_t p2, uint8_t *dataBuffer, uint16_t dataLength, volatile unsigned int *flags, volatile unsigned int *tx);
 
 #endif
